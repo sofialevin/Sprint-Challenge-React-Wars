2a98df51a6078434ddeb44f2191c18b298196e23
sofialevin <slevin@M-4876.local>
2019-08-03 09:31:21 -0700

    Completed basic component set up.

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './App.css';
 
+import CardList from './components/CardList'
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -26,6 +28,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <CardList />
     </div>
   );
 }
